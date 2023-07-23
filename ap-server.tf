088a3757c8a249e22ae61d444b63064ea89f8b7e
nishi-2400 <a7w210872424bump@gmail.com>
2023-07-23 15:15:05 +1000

    ap server setup

---
+++ ap-server.tf
@@ -0,0 +1,36 @@
+# ---------------------------------
+# EC2 App Server
+# ---------------------------------
+
+# Key pair
+resource "aws_key_pair" "keypair" {
+  key_name   = "${var.project}-${var.environment}-kaypair"
+  public_key = file("./src/aws-basic-dev-keypair.pub")
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-kaypair"
+    Project = var.project
+    Env     = var.environment
+  }
+
+}
+
+# EC2 Instance
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet-1a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id,
+    aws_security_group.opmng_sg.id
+  ]
+  key_name = aws_key_pair.keypair.key_name
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-ec2"
+    Project = var.project
+    Env     = var.environment
+    Type    = "app"
+  }
+}
--- data.tf
+++ data.tf
@@ -1,3 +1,24 @@
 data "aws_prefix_list" "s3_pl" {
   name = "com.amazonaws.*.s3" # Region Changable
 }
+
+# GET AMI ID
+data "aws_ami" "app" {
+  most_recent = true
+  owners      = ["self", "amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
