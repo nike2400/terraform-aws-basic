4e9a3a946a36566c157f1f1dd103833f92e6b6b4
nishi-2400 <a7w210872424bump@gmail.com>
2023-07-31 12:53:13 +1000

    ssm parameter store set up

--- ap-server.tf
+++ ap-server.tf
@@ -36,3 +36,37 @@ resource "aws_instance" "app_server" {
     Type    = "app"
   }
 }
+
+# ---------------------------------
+# SSM Parameter Store
+# ---------------------------------
+# Error (AccessDeniedException) occurs if you have "aws" prefix in the name of parameter
+resource "aws_ssm_parameter" "host" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_HOST"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.address
+}
+
+resource "aws_ssm_parameter" "port" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_PORT"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.port
+}
+
+resource "aws_ssm_parameter" "database" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_DATABASE"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.name #Databasae = name
+}
+
+resource "aws_ssm_parameter" "username" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_USERNAME"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.username
+}
+
+resource "aws_ssm_parameter" "password" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_PASSWORD"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.password
+}
