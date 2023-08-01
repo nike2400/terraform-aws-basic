3c6da1441c1f1a7d951c6a2510a2a7335bd11508
nishi-2400 <a7w210872424bump@gmail.com>
2023-08-01 13:26:33 +1000

    alb set up

---
+++ alb.tf
@@ -0,0 +1,46 @@
+# ---------------------------------
+#  Application Load Balancer
+# ---------------------------------
+
+resource "aws_lb" "alb" {
+  name               = "${var.project}-${var.environment}-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = [
+    aws_security_group.web_sg.id
+  ]
+  subnets = [
+    aws_subnet.public_subnet-1a.id,
+    aws_subnet.public_subnet-1c.id,
+  ]
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "${var.project}-${var.environment}-app-alb-tg"
+  port     = 3000 # Node.js
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-alb-tg"
+    Project = var.project
+    Env     = var.environment
+  }
+
+}
+
+# associate Instance with elb target group
+resource "aws_lb_target_group_attachment" "alb_instance" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.app_server.id # EC2 instance id  
+}
+
+resource "aws_lb_listener" "alb_listener_http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80 # port allowed
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+} 
