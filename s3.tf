3cca0f7c63a2aa13adaef934c2354219da20021e
nishi-2400 <a7w210872424bump@gmail.com>
2023-08-06 12:19:47 +1000

    cloudftont for alb and s3

--- s3.tf
+++ s3.tf
@@ -39,7 +39,7 @@ resource "aws_s3_bucket_public_access_block" "s3_static_bucket_block" {
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = false
+  restrict_public_buckets = true
 }
 
 data "aws_iam_policy_document" "s3_static_bucket_policy_doc" {
@@ -48,8 +48,8 @@ data "aws_iam_policy_document" "s3_static_bucket_policy_doc" {
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.s3_static_bucket.arn}/*"]
     principals {
-      type        = "*"
-      identifiers = ["*"]
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cloud_front_oai.iam_arn]
     }
   }
 }
